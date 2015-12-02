4438e9713af05e4de25a68269d5ffad56652ea94
Rachel Ehrlich <rae84@cornell.edu>
2015-12-02 17:40:29 -0500

    update get_data script to create directory

--- get_data.py
+++ get_data.py
@@ -1,4 +1,7 @@
-import urllib2
+import os, urllib2
+
+if not os.path.exists('data'):
+    os.makedirs('data')
 
 url = "https://data.cityofnewyork.us/api/views/43nn-pn8j/rows.csv?accessType=DOWNLOAD"
 req = urllib2.Request(url)
