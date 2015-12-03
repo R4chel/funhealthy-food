0f3c2c956f6eef1eec3471dfcf6d0c5862362450
Rachel Ehrlich <rae84@cornell.edu>
2015-12-03 15:30:02 -0500

    start cleaning data

--- sanitize_data.py
+++ sanitize_data.py
@@ -1,8 +1,14 @@
 import pandas as pd
+import re
 
 def load_data():
     with open('data/doh_inspections.csv', 'r') as f:
         data = pd.read_csv(f, quotechar='"')
+    data = data[~data['INSPECTION DATE'].isin(['01/01/1900'])]
     return data
 
+def make_violation_dict(data):
+    return dict(zip(data['VIOLATION CODE'], data['VIOLATION DESCRIPTION'])
 
+data = load_data()
+violation_dict = make_violation_dict(data)
