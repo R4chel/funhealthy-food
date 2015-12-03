328ce9171585d70f4182f3d2c73d618ba4f2f4cb
Rachel Ehrlich <rae84@cornell.edu>
2015-12-03 15:45:18 -0500

    update get data to download legally operating business data

--- get_data.py
+++ get_data.py
@@ -3,9 +3,14 @@ import os, urllib2
 if not os.path.exists('data'):
     os.makedirs('data')
 
-url = "https://data.cityofnewyork.us/api/views/43nn-pn8j/rows.csv?accessType=DOWNLOAD"
-req = urllib2.Request(url)
-response = urllib2.urlopen(req)
-text = response.read()
-with open('data/doh_inspections.csv', 'w') as f:
-    f.write(text)
+def dl_file(url, f_name):
+   req = urllib2.Request(url)
+   response = urllib2.urlopen(req)
+   text = response.read()
+   with open(f_name, 'w') as f:
+       f.write(text)
+
+dl_file('https://data.cityofnewyork.us/api/views/43nn-pn8j/rows.csv?accessType=DOWNLOAD',
+        'data/doh_inspections.csv')
+dl_file('https://data.cityofnewyork.us/api/views/w7w3-xahh/rows.csv?accessType=DOWNLOAD',
+        'data/lob.csv')
